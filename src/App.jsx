8368b831fa2651825d684025cabb95188a2ae165
Maxeemjaa <maxeemja.inc@gmail.com>
2022-04-02 15:11:27 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Container, Stack, Button } from 'react-bootstrap';
+import BudgetCard from './components/BudgetCard';
+function App() {
+	return (
+		<Container className='my-4'>
+			<Stack direction='horizontal' gap='2' className='mb-4'>
+				<h1 className='me-auto'>Budgets</h1>
+				<Button variant='primary'>Add budget</Button>
+				<Button variant='outline-primary'>Add expense</Button>
+			</Stack>
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+					gap: '1rem',
+					alignItems: 'flex-start'
+				}}
+			>
+        <BudgetCard name="Entertainment" amount="600" max="1000">
+
+        </BudgetCard>
+      </div>
+		</Container>
+	);
+}
+
+export default App;
